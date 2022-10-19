6b848ac9debf9a4cc9beda73de4d8b9d6f0fa346
KashbullinRail <kashbullinrail@gmail.com>
2022-10-19 13:35:52 +0300

    реализация функции init

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -2,20 +2,24 @@ package com.example.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.recyclerviewexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val adapter = PictureAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
     }
  private fun init() {
      binding.apply {
-         rvPicture
+         rvPicture.layoutManager = GridLayoutManager(this@MainActivity, 3)
+         rvPicture.adapter = adapter
      }
 
  }
